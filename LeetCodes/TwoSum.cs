1b2b830d1abfdd060e530136f6a03e52a4605819
Mateus Cizeski <mateuscizeski@gmail.com>
2024-10-31 22:43:40 -0300

    leetcode twosum

---
+++ LeetCodes/TwoSum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    internal class TwoSumClass
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+
+                if (map.ContainsKey(diff))
+                {
+                    return new int[] { map[diff], i };
+                }
+
+                map[nums[i]] = i;
+            }
+
+            throw new ArgumentException("Nenhuma solução encontrada, embora o problema garanta uma.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RunTwoSumExample();
+        }
+
+        static void RunTwoSumExample()
+        {
+            var twoSum = new TwoSumClass();
+
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 26;
+            int[] result = twoSum.TwoSum(nums, target);
+
+            Console.WriteLine($"Índices encontrados: {result[0]}, {result[1]}");
+            Console.ReadLine();
+        }
+    }
+}
