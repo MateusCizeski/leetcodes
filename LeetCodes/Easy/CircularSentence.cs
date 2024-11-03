2d0ba6af7790e26108eb1a45b43789198faf9a03
Mateus Cizeski <mateuscizeski@gmail.com>
2024-11-03 10:14:58 -0300

    leetcode de frases com sentença circular.

--- LeetCodes/Easy/CircularSentence.cs
+++ LeetCodes/Easy/CircularSentence.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,26 @@ namespace LeetCode.LeetCodes.Easy
         public bool CircularSentence(string text)
         {
             string[] textSplit = text.Split(' ');
+            char primeiroChar = textSplit[0][0];
+            char ultimoChar = textSplit[textSplit.Length - 1][textSplit[textSplit.Length - 1].Length - 1];
 
-            foreach (string s in textSplit)
+            if(primeiroChar != ultimoChar)
             {
-                Console.WriteLine(s);
+                return false;
             }
 
-            return false;
+            for(int i = 0; i < textSplit.Length - 1; i++)
+            {
+                char ultimoCharPalavraAtual = textSplit[i][textSplit[i].Length - 1];
+                char primeiroCharProximaPalavra = textSplit[i + 1][0];
+
+                if(ultimoCharPalavraAtual != primeiroCharProximaPalavra)
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
