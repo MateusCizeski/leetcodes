d67868bcb94fbc291b028b028edb5717efb00404
mateu <mateuscizeski@gmail.com>
2024-12-18 19:46:36 -0300

    leetocode ClimbStairs.

--- LeetCodes/Easy/ClimbStairs.cs
+++ LeetCodes/Easy/ClimbStairs.cs
@@ -4,7 +4,20 @@
     {
         public int ClimbStairs(int n)
         {
+            if (n <= 2) return n;
 
+            int a = 1;
+            int b = 2;
+            int result = 0;
+
+            for (int i = 3; i <= n; i++)
+            {
+                result = a + b;
+                a = b;
+                b = result;
+            }
+
+            return result;
         }
     }
 }
