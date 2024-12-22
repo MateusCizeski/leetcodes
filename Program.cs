d0080a185361905c5e169ff151ae401b9dd15e48
mateu <mateuscizeski@gmail.com>
2024-12-22 14:17:08 -0300

    leetcode merge array

--- Program.cs
+++ Program.cs
@@ -268,5 +268,21 @@ namespace LeetCode
             Console.ReadLine();
         }
         #endregion
+
+        #region MergeArrays
+        static void MergeSortedArray()
+        {
+            MergeSortedArray mergeSortedArray = new MergeSortedArray();
+            int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+            int m = 3;
+            int[] nums2 = { 2, 5, 6 };
+            int n = 3;
+
+            int result = mergeSortedArray.Merge(nums1, m, nums2, n);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        #endregion
     }
 }
